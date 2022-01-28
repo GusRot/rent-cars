001f54e78844d6d46e3b8d059bf798f3a6a94e4a
Gustavo Rotta <gustavo.rotta@hotmail.com>
2022-01-28 16:24:14 -0300

    swagger-docs

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,12 @@
-import { Router } from "express";
+import { response, Router } from "express";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoryController } from "../modules/cars/useCases/listCategories";
+import multer from "multer";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 
 const categoriesRoutes = Router();
+
+const upload = multer({ dest: "./tmp" });
 
 categoriesRoutes.post("/", (req, res) => {
     return createCategoryController.handle(req, res);
@@ -10,6 +14,10 @@ categoriesRoutes.post("/", (req, res) => {
 
 categoriesRoutes.get("", (req, res) => {
     return listCategoryController.handle(req, res);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+    return importCategoryController.handle(req, res);
 });
 
 export { categoriesRoutes };
